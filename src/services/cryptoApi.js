5a9bf0aaff6e8201fabcc8c1a61bea68861ee166
Ervin Kacar <kacarervin@gmail.com>
2022-09-16 11:00:29 +0200

    Finish redux fetch logic

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "X-RapidAPI-Key": "4375993566msh52b02adc0dd8241p1639d4jsn863d198205bf",
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
-const baseUrl = "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd";
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
@@ -14,7 +14,8 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest("/exchanges"),
+      query: () => createRequest("/coins"),
     }),
   }),
 });
+export const { useGetCryptosQuery } = cryptoApi;
