3edf3ab5e8db521ba780872a9a594a402905cff6
kacarervin <kacarervin@gmail.com>
2022-10-06 13:52:13 +0200

    Finish

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+} from "chart.js";
 const { Title } = Typography;
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Tooltip
+);
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
@@ -17,6 +32,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
     );
   }
+
   const data = {
     labels: coinTimestamp,
     datasets: [
@@ -32,13 +48,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
   const options = {
     scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
+      y: {
+        ticks: {
+          beginAtZero: true,
         },
-      ],
+      },
     },
   };
 
@@ -63,87 +77,3 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 };
 
 export default LineChart;
-
-// import React from "react";
-// import { Line } from "react-chartjs-2";
-// import { Col, Row, Typography } from "antd";
-// import {
-//   Chart as ChartJS,
-//   PointElement,
-//   LineElement,
-//   Tooltip,
-//   Filler,
-//   CategoryScale,
-//   LinearScale,
-// } from "chart.js";
-
-// const { Title } = Typography;
-
-// ChartJS.register(
-//   PointElement,
-//   LineElement,
-//   Tooltip,
-//   Filler,
-//   CategoryScale,
-//   LinearScale
-// );
-
-// const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-//   const coinPrice = [];
-//   const coinTimestamp = [];
-
-//   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-//     coinPrice.push(coinHistory?.data?.history[i].price);
-//     coinTimestamp.push(
-//       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
-//     );
-//   }
-
-//   console.log("history", coinHistory);
-
-//   const data = {
-//     labels: coinTimestamp,
-//     dataset: [
-//       {
-//         label: "Price in USD",
-//         data: coinPrice,
-//         fill: false,
-//         backgroundColor: "#0071bd",
-//         borderColor: "#0071bd",
-//       },
-//     ],
-//   };
-
-//   const options = {
-//     scales: {
-//       yAxes: [
-//         {
-//           ticks: {
-//             beginAtZero: true,
-//           },
-//         },
-//       ],
-//     },
-//   };
-
-//   return (
-//     <>
-//       <Row className="chart-header">
-//         <Title className="chart-title" level={2}>
-//           {coinName} Price Chart
-//         </Title>
-//         <Col className="price-container">
-//           <Title level={5} className="price-change">
-//             {coinHistory?.data?.change}%
-//           </Title>
-//           <Title level={5} className="current-price">
-//             Current {coinName} Price: $ {currentPrice}
-//           </Title>
-//         </Col>
-//       </Row>
-//       <Line data={data} options={options} />
-//     </>
-//   );
-// };
-
-// export default LineChart;
